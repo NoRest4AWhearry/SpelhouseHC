31c72199647280a923387132d4519c7fdef6fe61
Allen Whearry <awhearry@gmail.com>
2016-10-25 00:29:24 -0400

    first commit

---
+++ js/shhc.js
@@ -0,0 +1,14 @@
+// Slider
+  $(function() {
+    $(".slider").responsiveSlides({
+        maxwidth: 970,
+		timeout: 3000,
+        speed: 800
+      })
+  });
+//fitVids
+ $(document).ready(function(){
+    // Target your .container, .wrapper, .post, etc.
+    $(".innerColBoxMain").fitVids();
+  });
+//Countdown
